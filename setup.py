00a1adc978504e87e015f704ed5f2287a5a95805
Keigh Rim <keigh.rim@gmail.com>
2021-07-08 14:50:35 -0400

    fixed bug in pip-installing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as requirements:
 
 setup(
     name='concatrim',
-    version='1.0.0',
+    version='1.0.1',
     python_requires='>=3',
     packages=['concatrim'],
     url='https://github.com/keighrim/concatrim',
